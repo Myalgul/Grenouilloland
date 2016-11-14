8d5f340b74cbfb637fa79552977d6f3ca179a40c
Zishiji <alex.lecleir@gmail.com>
2016-11-14 18:52:55 +0100

    Oubli du client

--- client.cpp
+++ client.cpp
@@ -1,97 +1,63 @@
-
-/**
- * @mainpage
- *
- * Le jeu de la grenouille.
- *
- * @author Alex LECLEIR Valentin LEFEBVRE
- */
-
-#include "presentateur/Presentateur.hpp"
 #include <iostream>
-#include <sstream>
 #include <cstdlib>
 
-/**
- * Programme principal.
- *
- * @param[in] argc - le nombre d'arguments de la ligne de commandes.
- * @param[in] argv - les arguments de la ligne de commandes.
- * @return @c EXIT_SUCCESS en cas d'exécution réussie ou @c EXIT_FAILURE sinon.
- */
-int
-main(int argc, char* argv[]) {
+#include "modele/Grenouilloland.hpp"
+#include "modele/Compteur.hpp"
 
-  // La ligne de commandes est vide : l'utilisateur demande de l'aide.
-  if (argc == 1) {
-    std::cout << "Usage: "
-	      << argv[0]
-	      << "dimension_min dimension_max dimension_initiale"
-	      << std::endl;
+void Affichage (const Grenouilloland& jeu);
+
+using namespace std;
+
+int main()
+{
+    char choix;
+    Grenouilloland jeu(5);
+    Grenouille* grenouille = jeu.lireGrenouille();
+    Compteur compteur;
+
+    do {
+        Affichage(jeu);
+        //compteur.demarrer(&jeu);
+        cout << grenouille->lireLigne() << "/" << grenouille->lireColonne() << ":" << grenouille->lireVie() << endl;
+        cin >> choix;
+        switch (choix) {
+            case 'h' :  grenouille->ecrireLigne(grenouille->lireLigne()-1);
+                        break;
+            case 'b' :  grenouille->ecrireLigne(grenouille->lireLigne()+1);
+                        break;
+            case 'g' :  grenouille->ecrireColonne(grenouille->lireColonne()-1);
+                        break;
+            case 'd' :  grenouille->ecrireColonne(grenouille->lireColonne()+1);
+                        break;
+        }
+        jeu.modification();
+        jeu.maj();
+    } while (!jeu.gagner() && !jeu.perdu());
+//    int i = 0;
+//    do {
+//        cout << "Generation " << i << endl;
+//        Affichage(jeu);
+//        cout << endl;
+//        compteur.demarrer(&jeu);
+//        compteur.reset();
+//        i++;
+//    } while (i != 10);
+
+    if (jeu.gagner()) {
+        cout << endl << "Bravo vous avez fini le jeu grenouilloland !!!" << endl;
+    } else {
+        cout << endl << "Mince votre grenouille est morte avant la fin, vous pouvez retenter votre chance" << endl;
+    }
+
     return EXIT_SUCCESS;
-  }
+}
 
-  // Le nombre d'arguments de la ligne de commandes est différent de quatre :
-  // l'utilisateur fait n'importe quoi.
-  if (argc != 4) {
-    std::cerr << "Nombre d'arguments incorrect." << std::endl;
-    return EXIT_FAILURE;
-  }
-
-  // Tentative d'extraction de la résolution minimum.
-  int minimum;
-  {
-    std::istringstream entree(argv[1]);
-    entree >> minimum;
-    if (! entree || ! entree.eof() || minimum < 1) {
-      std::cerr << "La résolution minimum n'est pas un entier positif."
-		<< std::endl;
-      return EXIT_FAILURE;
+void Affichage (const Grenouilloland& jeu) {
+    int dim = jeu.lireDimension();
+    for (int i = 0; i < dim; i++) {
+        for (int j = 0; j < dim; j++) {
+            cout << jeu.lireElement(i, j)->lireRepresentation() << " ";
+        }
+        cout << endl;
     }
-  }
-
-  // Tentative d'extraction de la résolution maximum.
-  int maximum;
-  {
-    std::istringstream entree(argv[2]);
-    entree >> maximum;
-    if (! entree || ! entree.eof() || maximum < minimum) {
-      std::cerr << "La résolution maximum n'est pas un entier supérieur à "
-		<< minimum
-		<< "."
-		<< std::endl;
-      return EXIT_FAILURE;
-    }
-  }
-
-  // Tentative d'extraction de la résolution initiale.
-  int initiale;
-  {
-    std::istringstream entree(argv[3]);
-    entree >> initiale;
-    if (! entree || ! entree.eof() || initiale < minimum ||
-        initiale > maximum) {
-      std::cerr << "La résolution maximum n'est pas un entier dans ["
-		<< minimum
-		<< ", "
-		<< maximum
-		<< "]."
-		<< std::endl;
-      return EXIT_FAILURE;
-    }
-  }
-
-  // Tout est ok : initialisation de Gtk.
-  Gtk::Main foo(argc, argv);
-
-  // Initialisation de la classe Vue.
-  Vue::initialiser();
-
-  // Instanciation et démarrage du présentateur.
-  Presentateur presentateur(minimum, maximum, initiale);
-  presentateur.demarrer();
-
-  // Tout s'est bien passé.
-  return EXIT_SUCCESS;
-
 }
